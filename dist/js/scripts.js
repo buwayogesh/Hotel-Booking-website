1463e8f729cdb9e8a9fca0d1c3073101bad208dc
gitanjali girigosavi <gitanjali.girigosavi@sadhanaitsolutions.com>
2022-08-02 12:26:55 +0530

    availabilitys status

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -3,5 +3,28 @@
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+document.querySelector('.btn').addEventListener('click',function(e){
+    document.querySelector('.text-center').remove();
+//    var input=document.createElement('input');
+//    input.setAttribute('type','date');
+//    document.querySelector('.container1').appendChild(input);
+      var html='';
+      var form=document.createElement('form');
+      form.id='checkavlbl';
+      
+      html += '<div class="d-flex justify-content-around">'
+      
+      html += '<div>'
+      html += '<label class="label1"><b>Check In:</b></label>'
+      html += '<input type="date" class="input1">'
+      html += '</div>'
+      html += '<div>'
+      html += '<label class="label2"><b>Check out:</b></label>'
+      html += '<input type="date" class="input2">'
+      html += '</div>'
+      html += '<button>Check Availability</button>'
+      html += '</div>'
+      
+      form.innerHTML=html;
+      document.querySelector('.container1').appendChild(form);
+})
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,25 @@
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+document.querySelector('.btn').addEventListener('click',function(e){
+    document.querySelector('.text-center').remove();
+//    var input=document.createElement('input');
+//    input.setAttribute('type','date');
+//    document.querySelector('.container1').appendChild(input);
+      var html='';
+      var form=document.createElement('form');
+      form.id='checkavlbl';
+      
+      html += '<div class="d-flex justify-content-around">'
+      
+      html += '<div>'
+      html += '<label class="label1"><b>Check In:</b></label>'
+      html += '<input type="date" class="input1">'
+      html += '</div>'
+      html += '<div>'
+      html += '<label class="label2"><b>Check out:</b></label>'
+      html += '<input type="date" class="input2">'
+      html += '</div>'
+      html += '<button>Check Availability</button>'
+      html += '</div>'
+      
+      form.innerHTML=html;
+      document.querySelector('.container1').appendChild(form);
+})
