0457ef79e8212edba4abdc4c26d248216391a99f
gitanjali girigosavi <gitanjali.girigosavi@sadhanaitsolutions.com>
2022-08-05 15:02:33 +0530

    changes

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -4,30 +4,37 @@
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
 */
 document.querySelector('.btn.bookbtn').addEventListener('click',function(e){
-    document.querySelector('.div1').remove();
+    document.querySelector('.booking').remove();
     document.querySelector('.navbar').remove();
-//    var input=document.createElement('input');
-//    input.setAttribute('type','date');
-//    document.querySelector('.container1').appendChild(input);
+//    
       var html='';
-      var form=document.createElement('form');
-      form.id='checkavlbl';
+      var card=document.createElement('div');
+      card.id='checkavlbl';
+      card.className="w-25"
       
-    //   html += '<div class="d-flex justify-content-around">'
-      html += '<div class="card">'
+    
+      
+      html += '<div class="card check-avail-card opacity-75">'
       html += '<div class="card-body">'
-      html += '<img src="./assets/logo.jpg" alt="logo" style="height: 100px; width: 100px" />'
+      html += '<img src="./assets/logo.jpg" alt="logo" class="w-25 h-25"/>'
       html += '<div class="d-flex justify-content-between">'
-      html += '<label class="label1 col-sm-5 col-form-label"><b>Check In:</b></label>'
-      html += '<input type="date" class="input1">'
+      html += '<label class="label1"><b>Check In:</b></label>'
+      html += '<input type="date" class="w-50 startDate" onchange="checkDate()">'
       html += '</div><br>'
       html += '<div class="d-flex justify-content-between">'
-      html += '<label class="label2 col-sm-5 col-form-label"><b>Check out:</b></label>'
-      html += '<input type="date" class="input2">'
+      html += '<label class="label2"><b>Check out:</b></label>'
+      html += '<input type="date" class="w-50">'
       html += '</div><br>'
-      html += '<button class="ckavailbtn"><strong>Check Availability</strong></button>'
+      html += '<button class="check-available-btn w-100 rounded lh-lg"><strong>Check Availability</strong></button>'
+     
+      html += '<button class="next" onclick="buildUserInfo()"><strong>Next</strong></button>'
+    
       html += '</div>'
-      html += '</div>'
-      form.innerHTML=html;
-      document.querySelector('.container1').appendChild(form);
+      card.innerHTML=html;
+      document.querySelector('.container1').appendChild(card);
+
+     
 })
+
+
+
